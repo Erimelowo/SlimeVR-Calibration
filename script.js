069877b9adb358b41220be9eeee081be93eac7a2
Louka <marioluigivideo@gmail.com>
2022-03-17 22:59:35 -0400

    side menu support for phone
    start with side menu closed if on phone and doesn't push content when open

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 const sideMenu = document.getElementById("sideMenu");
-sideMenu.style.width = "220px";
 const mainHolder = document.getElementById("holderMain");
-mainHolder.style.marginLeft = "234px";
+const menuToggleButton = document.getElementById("navToggle");
+let sideMenuOn;
+
+if(window.innerWidth / window.innerHeight < 1){
+    disableNav();
+}
+else{
+    enableNav();
+}
 
 function toggleNav() {
-    if(sideMenu.style.width == "220px"){
-        sideMenu.style.width = "0px";
-        mainHolder.style.marginLeft = "16px";
+    if(sideMenuOn){
+        disableNav();
     }
     else{
-        sideMenu.style.width = "220px";
+        enableNav();
+    }
+}
+
+function disableNav(){
+    sideMenuOn = false;
+    sideMenu.style.width = "0px";
+    mainHolder.style.marginLeft = "16px";
+    navToggle.style.marginLeft = "10px";
+}
+function enableNav(){
+    sideMenuOn = true;
+    sideMenu.style.width = "220px";
+    if((window.innerWidth / window.innerHeight > 1)){
         mainHolder.style.marginLeft = "234px";
+    }
+    else{
+        navToggle.style.marginLeft = "224px";
     }
 }
