f7211770e2de85284351bfe8258d0f77cbed6e5d
Erimel <marioluigivideo@gmail.com>
2022-03-24 10:51:17 -0400

    add visual joints

--- slimevrSkeleton.js
+++ slimevrSkeleton.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById("skeletonCanvas");
+const inputs = document.getElementsByClassName(".numberInput");
 
 let head = 10
 let neck = 10;
@@ -9,9 +10,7 @@ let hipsWidth = 26;
 let legs = 90;
 let knees = 50;
 
-let controllerY = 5, controllerZ = 15, elbowDistance = 24;
-
-let currentX = canvas.width/2, currentY = 10;
+let currentX = canvas.width/2, currentY = 50;
 
 const scale = 3;
 const width = 8;
@@ -23,26 +22,37 @@ function updateSkeleton(){
         const ctx = canvas.getContext('2d');
 
         // Head and spine
+        drawCircle(ctx, [currentX + head * scale, currentY], 2, 'purple');
         drawLine(ctx, [currentX + head * scale, currentY], [currentX, currentY], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
         drawLine(ctx, [currentX, currentY], [currentX, currentY + neck * scale], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
         drawLine(ctx, [currentX, currentY], [currentX, currentY + chest * scale], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
         drawLine(ctx, [currentX, currentY], [currentX, currentY + (torso - waist - chest) * scale], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
         drawLine(ctx, [currentX, currentY], [currentX, currentY + waist * scale], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
 
         // Hips
         drawLine(ctx, [currentX, currentY], [currentX + hipsWidth/2 * scale, currentY], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
         drawLine(ctx, [currentX - hipsWidth/2 * scale, currentY], [currentX - hipsWidth * scale, currentY], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
 
         // Right leg
         drawLine(ctx, [currentX + hipsWidth * scale, currentY], [currentX + hipsWidth * scale, currentY + (legs - knees) * scale], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
         drawLine(ctx, [currentX, currentY], [currentX, currentY + knees * scale], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
 
         // Left leg
         drawLine(ctx, [currentX - hipsWidth * scale, currentY - legs * scale], [currentX - hipsWidth * scale, currentY - knees * scale], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
         drawLine(ctx, [currentX, currentY], [currentX, currentY + knees * scale], 'purple', width);
+        drawCircle(ctx, [currentX, currentY], 2, 'purple');
     }
 }
-
 function drawLine(ctx, begin, end, stroke, width) {
     if (stroke) {
         ctx.strokeStyle = stroke;
@@ -56,5 +66,17 @@ function drawLine(ctx, begin, end, stroke, width) {
     ctx.beginPath();
     ctx.moveTo(...begin);
     ctx.lineTo(...end);
+    ctx.stroke();
+}
+function drawCircle(ctx, begin, radius, stroke) {
+    if (stroke) {
+        ctx.strokeStyle = stroke;
+    }
+    if (width) {
+        ctx.lineWidth = width;
+    }
+    ctx.lineCap = "round";
+    ctx.beginPath();
+    ctx.arc(begin[0], begin[1], radius, 0, 2 * Math.PI);
     ctx.stroke();
 }
